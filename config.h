48db6d09d68333f03c81045a699d55c5f9f7e407
Lew Dixon <ldixon.design@gmail.com>
2022-04-19 11:55:43 -0400

    added winicon

--- config.h
+++ config.h
@@ -16,6 +16,8 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+#define ICONSIZE 16   /* icon size */
+#define ICONSPACING 6 /* space between icon and title */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
