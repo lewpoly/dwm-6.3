6d556811dda7ced1bf79201d49cd3881c254e6cf
lewpoly <ldixon.design@gmail.com>
2022-06-04 22:57:14 -0400

    changed gap size, changed volume binds

--- config.h
+++ config.h
@@ -11,14 +11,14 @@ static const unsigned int snap      = 10;       /* snap pixel */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const unsigned int gappih    = 22;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 22;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 22;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 22;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 17;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 17;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 17;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 17;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 #define ICONSIZE 16   /* icon size */
 #define ICONSPACING 6 /* space between icon and title */
-static const char *fonts[]          = { "MesloLGS Nerd Font:size=10", "JoyPixels:size=11" };
+static const char *fonts[]          = { "FiraCode Nerd Font:size=14", "JoyPixels:size=14" };
 static const char *brupcmd[]            = { "brightnessctl", "set", "10%+", NULL };
 static const char *brdowncmd[]          = { "brightnessctl", "set", "10%-", NULL };
 /* static const char *upvol[]              = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",     NULL }; */
@@ -135,15 +135,15 @@ static Key keys[] = {
 	{ MODKEY,                       XK_space,                  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_f,                      togglefloating, {0} },
 	{ MODKEY,                       XK_s,                      togglesticky,   {0} },
-	{ MODKEY,                       XK_0,                      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,                      tag,            {.ui = ~0 } },
+	/* { MODKEY,                       XK_0,                      view,           {.ui = ~0 } }, */
+	/* { MODKEY|ShiftMask,             XK_0,                      tag,            {.ui = ~0 } }, */
 	{ MODKEY,                       XK_comma,                  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period,                 focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,                  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period,                 tagmon,         {.i = +1 } },
-	{ 0,                            XK_F12,                    spawn,          SHCMD("changevolume up") },
- 	{ 0,                            XK_F11,                    spawn,          SHCMD("changevolume down") },
-	{ 0,                            XK_F10,                    spawn,          SHCMD("changevolume mute") },
+	{ MODKEY,                       XK_equal,                  spawn,          SHCMD("changevolume up") },
+ 	{ MODKEY,                       XK_minus,                  spawn,          SHCMD("changevolume down") },
+	{ MODKEY,                       XK_0,                      spawn,          SHCMD("changevolume mute") },
 	{ 0,                            XF86XK_AudioRaiseVolume,   spawn,          SHCMD("changevolume up") },
 	{ 0,                            XF86XK_AudioLowerVolume,   spawn,          SHCMD("changevolume down") },
 	{ 0,                            XF86XK_AudioMute,          spawn,          SHCMD("changevolume mute") },
