ddcd01573ffff20861ff28f015f9629be9fb7978
lewpoly <ldixon.design@gmail.com>
2022-07-02 21:41:45 -0400

    seems alot

--- config.h
+++ config.h
@@ -5,12 +5,12 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14" };
-static const char dmenufont[]       = "monospace:size=14";
-static const char col_gray1[]       = "#000";
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#d4d4d4";
-static const char col_gray4[]       = "#fff";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const unsigned int baralpha = 0x70;
 static const unsigned int borderalpha = OPAQUE;
